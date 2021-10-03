31c5fdb08e3eb075767eb1354ecef718a0031e5b
KhalaRKS <albanokainer@hotmail.com>
2021-10-03 08:07:39 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Hola, soy Albano Kainer y soy desarrollador web junior. Te invito a mi página para que conozcas mas sobre mí."/>
+    <meta name="keywords" content="Desarrollo, web, diseño, programador, junior, IT, sistemas, developer, argentina, buenos aires, CABA, informatica, trainee, programacion, develop"/>
+    <meta name="robots" content="index,follow" />
+
+    <!-- META TAG FACEBOOK CARD SHARE -->
+    <meta property="og:title" content="Albano Kainer personal portfolio">
+    <meta property="og:description" content="Hi i'm a junior web developer and i am inviting you to look my personal web!">
+    <meta property="og:image" content="https://albanokainer.com/imagenes/foto-perfil.jpg">
+    <meta property="og:url" content="https://albanokainer.com">
+    <meta name="twitter:card" content="summary_short_image">
+    <link rel="icon" type="image/png" href="imagenes/iconos/favicon.png" sizes="60x60">
+
+
+    <link rel="stylesheet" href="index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,300;0,400;0,600;0,700;1,400&display=swap" rel="stylesheet">
+    <title>Albano Kainer Web designer</title>
+    <script src="https://unpkg.com/scrollreveal"></script>
+
+</head>
+<body id="body">
+ 
+    <!--Section header-->
+    <div class="contenedor-general">
+
+        <header class="contenedor-header">
+            <nav class="contenedor-navbar">
+                <ul class="contenedor-buscador-navbar">
+                    <li class="list-item-navbar li-first"><a href="#aboutme">About me</a></li>
+                    <li class="list-item-navbar li-second"><a href="#contact">Contact me</a></li>
+                    <li class="list-item-navbar li-third"><a href="#projects">Projects</a></li>
+                    <li class="list-item-navbar" id="language-display">Language
+                        <ul class="contenedor-language">
+                            <li class="list-item-navbar"><a href="espanol.html">Spanish</a></li>
+                            <li class="list-item-navbar">English</li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+            <section class="contenedor-section-info-header">
+                <div class="contenedor-textos-header">
+                    <h2>I'm <span id="animacion-uno">Albano</span> Kainer </h2>
+                    <p><span id="animacion-tres">an</span> Argentinian</p>
+                    <h1>Full <span id="animacion-cuatro">stack</span>  web <span id="hashtag-header">#</span> <span id="animacion-dos">developer</span></h1>
+                    <a href="#aboutme" id="pointer"><button id="boton-knowme-header">KNOW ME</button></a>
+                </div>
+            </section>
+        </header>
+        <main>
+            <!--Section About me-->
+                <section>
+                    <article class="contenedor-article-aboutme" id="aboutme">
+                            <h2>About  me</h2>
+                            <div class="contenedor-informacion-aboutme">
+
+                                <div class="foto-perfil-header">
+                                    <div ><img src="imagenes/foto-perfil.jpg" alt=""></div>
+                                
+                                </div>
+                                <div class="contenedor-redes-aboutme">
+                                    <a href="https://github.com/KhalaRKS"target="_blank"><img src="imagenes/iconos/github-logo.svg" class="iconos-redes cancel-margin" alt=""></a>
+                                    <a href="https://www.linkedin.com/in/albanokainer/"target="_blank"><img src="imagenes/iconos/linkedin-logo.svg" class="iconos-redes" alt=""></a>
+                                    <a href="https://www.instagram.com/alba_kainer/"target="_blank"><img src="imagenes/iconos/instagram-logo.svg" class="iconos-redes" alt=""></a>
+                                </div>
+                                <div class="contenedor-texto-aboutme">
+                                    <p><span id="first-letter-aboutme">H</span>i welcome to my webpage my name is Albano but you can also call me Alba. I am a technology lover
+                                    </p>
+                                    <p><span id="first-letter-aboutme">I</span> am a Systems Engineering student and at the same time I have been studying Web Programming for 1 year and I am looking for a job to be able to exceed goals and grow with the work team and personally.
+                                    </p>
+                                    <p><span id="first-letter-aboutme">I</span> invite you to continue sliding and learn a little more about my projects, knowledge and future learning goals that I plan to fulfill.
+                                    </p>
+                                </div>
+                                <div class="contenedor-botones-aboutme">
+                                        <a href="download/AlbanoLucasKainerCV.pdf" download="AlbanoLucasKainerCV.pdf" class="boton-aboutme" id="boton-download"><button class="boton-descarga">Download CV</button></a> 
+                                        <button class="boton-aboutme" id="boton-contactme">Contact me</button>
+                                </div>
+                                
+                                <div class="contenedor-iconos">
+                                    <h3 class="titulo-iconos">Technologies I work with</h3>
+                                    <img src="imagenes/iconos/css-logo.svg" class="iconos" alt="">
+                                    <img src="imagenes/iconos/js-logo.svg" class="iconos" alt="">
+                                    <img src="imagenes/iconos/html-logo.svg" class="iconos" alt="">
+                                    <img src="imagenes/iconos/sass-logo.svg" class="iconos" alt="">
+                                    <img src="imagenes/iconos/bs-logo.svg" class="iconos" alt="">
+                                    <img src="imagenes/iconos/git-logo.svg" class="iconos" alt="">
+                                    <h3 class="titulo-iconos">What I am learning? </h3>
+                                    <img src="imagenes/iconos/react-logo.svg" class="iconos" alt="">
+                                    <img src="imagenes/iconos/nodejs-logo.svg" class="iconos" alt="">
+                                    <img src="imagenes/iconos/mongodb-logo.svg" class="iconos" alt="">
+                                    <h3 class="titulo-iconos">What I would like to learn? </h3>
+                                    <img src="imagenes/iconos/angular-logo.svg" class="iconos" alt="">
+                                    <img src="imagenes/iconos/ts-logo.svg" class="iconos" alt="">
+                                    <img src="imagenes/iconos/python-logo.svg" class="iconos" alt="">
+                                </div>
+                            </div>
+                    </article>
+        
+                </section>
+                <!--Section Projects-->
+                <section class="contenedor-section-projects" id="projects">
+                    <h2>Projects I made</h2>
+                    <div class="contenedor-proyectos">
+                        <div class="proyectos">
+                            <div><p>a web calculator</p>
+                                <a class="boton-proyecto" target="_blank"  href="https://calculadora-dusky.vercel.app/">Watch project</a></div>
+                                <img src="imagenes/calculadora-proyecto.png" alt=""></div>
+                        <div class="proyectos"><div><p>md landing page</p>
+                            <a target="_blank"  href="https://navbar-khalarks.vercel.app/" class="boton-proyecto">Watch project</a></div>
+                        <img src="imagenes/md-proyecto.png" alt=""></div>
+                        <div class="proyectos"><div><p>Faster Aim</p>
+                            <a target="_blank"  href="https://minijuego-khalarks.vercel.app/" class="boton-proyecto">Watch project</a></div>
+                            <img src="imagenes/minijuego.jpg" alt="minijuego-imagen"></div>
+                            <div class="proyectos"><div><p>Integrity project</p>
+                                <a target="_blank"  href="https://integrador-khalarks.vercel.app/" class="boton-proyecto">Watch project</a></div>
+                            <img src="imagenes/integrador.jpg" alt=""></div>
+                        <div class="proyectos"><div><p>locked</p>
+                            <a target="_blank"  href="https://calculadora-dusky.vercel.app/" class="boton-proyecto">Stay tuned</a></div>
+                        <img src="imagenes/locked.png" alt=""></div>
+                        <div class="proyectos"><div><p>locked</p>
+                            <a target="_blank"  href="https://calculadora-dusky.vercel.app/" class="boton-proyecto">Stay tuned</a></div>
+                        <img src="imagenes/locked.png" alt=""></div>
+                    </div>
+                </section>
+                <!--Section Contact me -->
+                <section class="contenedor-section-contact" id="contact">
+                <?php 
+                $myemail = 'albanokainer@hotmail.com';
+                $name = $_POST['nombre'];
+                $email = $_POST['email'];
+                $message = $_POST['mensaje'];
+
+                $to = $myemail;
+                $email_subject = "Nuevo mensaje: $subject";
+                $email_body = "Haz recibido un nuevo mensaje. \n Nombre: $name \n Correo: $email \n Mensaje: \n $message";
+                $headers = "From: $email";
+
+                mail($to, $email_subject, $email_body, $headers);
+                echo "El mensaje se ha enviado correctamente, sera respondido lo antes posible.";
+                ?>
+
+                    <h2>Something caught your attention?</h2>
+                    <p>Feel free to contact me</p>
+                    <div class="contenedor-formulario">
+                        <form action="formulario.php" class="formulario" method="POST">
+                            <div>
+                                <span class="name">First name</span>
+                                <input  class="input-datos" type="text" name="name"required="required">
+                            </div>
+                            <div>
+                                <span class="name">Email</span>
+                                <input class="input-datos"  type="text" name="email" required="required">
+                            </div>
+                            <div>
+                                <span class="name" id="textname">Type your message here...</span>
+                                <textarea class="input-datos" id="textarea" id="" name="mensaje" cols="30" rows="10"></textarea>
+                            </div>
+                            <div>
+                                <input type="submit" id="boton-submit" value="Send">
+                            </div>
+                        </form>
+                    </div>
+                </section>
+        </main>
+        <footer>
+            <div class="contenedor-redes">
+                <p>My social networks</p>
+                <div>
+                    <a href="https://github.com/KhalaRKS"target="_blank"><img src="imagenes/iconos/github-logo.svg" class="iconos-redes cancel-margin" alt=""></a>
+                    <a href="https://www.linkedin.com/in/albanokainer/"target="_blank"><img src="imagenes/iconos/linkedin-logo.svg" class="iconos-redes" alt=""></a>
+                    <a href="https://www.instagram.com/alba_kainer/"target="_blank"><img src="imagenes/iconos/instagram-logo.svg" class="iconos-redes" alt=""></a>
+                </div>
+                    
+                <p id="footer-text">All rights reserved - Icon made by Free icons from www.freeicons.io</p>
+            </div>
+        </footer>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
